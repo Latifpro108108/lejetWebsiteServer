ae751c17bb9787027433c2fc3521ddf74d15340f
Latifpro108108 <dabonelati@gmail.com>
2024-12-03 21:01:28 +0000

    more commited out codes

--- server.js
+++ server.js
@@ -12,21 +12,22 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
-  credentials: true
-}));
+// app.use(cors({
+//   origin: process.env.CLIENT_URL || 'http://localhost:3000',
+//   credentials: true
+// }));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Logging middleware
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path}`, {
-    body: req.body,
-    auth: req.headers.authorization
-  });
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(`${req.method} ${req.path}`, {
+//     body: req.body,
+//     auth: req.headers.authorization
+//   });
+//   next();
+// });
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI, {
